cede205df5e2944a68da3ee32217c0ec5ea720d9
kurtis <kurtispedersen42@gmail.com>
2018-06-18 18:24:58 -0400

    moved script so it works

--- scripts/index.js
+++ scripts/index.js
@@ -9,63 +9,60 @@ var card = {
     front: 'question',
     back: 'answer',
 }
+
 var addQuestion = () => {                            //adding a question, must update question and answer array, should require an object as input
 
     document.querySelector('textarea').value = 'Enter Question?';
     var saveButton = document.querySelector('button.save');
     var cancelButton = document.querySelector('button.cancel');
     toggleVisibility();
-    var question = '';
+    
 
     saveButton.addEventListener('click', (e1) => {                      //1st degree save
-
+        var question = '';
         question = document.querySelector('textarea').value;
         console.log(question);
         document.querySelector('textarea').value = 'Enter Answer?';
-        var answer = '';
         var answerSave = document.querySelector('button.save');
         var answerCancel = document.querySelector('button.cancel');
+        e1.stopImmediatePropagation();
 
-        answerSave.addEventListener('click', (e1) => {                  //2nd degree save
+        answerSave.addEventListener('click', (e2) => {                  //2nd degree save
+            var answer = '';
             answer = document.querySelector('textarea').value;
             console.log(answer);
             toggleVisibility();
             document.querySelector('.ID').innerHTML = 'ID: ' + 'add code for array';        //add array stuff
-            e1.stopImmediatePropagation();
+            document.querySelector('textarea').value = 'Current Card Question';
+            e2.stopImmediatePropagation();
         });
 
-        answerCancel.addEventListener('click', (e1) => {                        //2nd degree cancel
+        answerCancel.addEventListener('click', (e2) => {                        //2nd degree cancel
             document.querySelector('.ID').innerHTML = 'ID: ' + 'Current ID'; 
             toggleVisibility();
-            e1.stopImmediatePropagation();
+            e2.stopImmediatePropagation();
         });
 
-    });
+    }); 
 
     cancelButton.addEventListener('click', (e1) => {                        //1st degree ancel
         toggleVisibility();
         document.querySelector('textarea').value = 'Previous Card'; //write code to return to old card here
         document.querySelector('.ID').innerHTML = 'ID: ' + 'Current ID'; //write code to old topic here
         e1.stopImmediatePropagation();
-    });
+    });  
+    
 
-    /*var superiorQ = numberQ + 1;      handle array stuff
-
-    for (n = 0; n <= superiorQ; n += 1) {
-        if (questionArray[n] == true) {
-        } else {
-            questionArray[n] = question;
-        }
-    } */
-
-    e.preventDefault;
 };
 
 var newTopicButton = document.querySelector('button[type=newTopic]');
 var newCardButton = document.querySelector('button[type=newCard]');
-//newCardButton.addEventListener('click', () => {
-//   addQuestion();
-//});
+
+newCardButton.addEventListener('click', (e) => {
+
+   addQuestion();
+   e.preventDefault();
+});
 
 newTopicButton.addEventListener('click', (e) => {                       //creats new topic folder
     document.querySelector('textarea').value = 'Define Topic?';         //everything good
@@ -93,7 +90,7 @@ newTopicButton.addEventListener('click', (e) => {                       //creats
         document.querySelector('.topic').innerHTML = 'Topic: ' + 'Previous Topic'; //write code to old topic here
         e1.stopImmediatePropagation();
     });
-    e.preventDefault;
+    e.preventDefault();
 });
 
 
@@ -148,7 +145,14 @@ var toggleVisibility = () => {
     }
 };
 
+/*var superiorQ = numberQ + 1;      handle array stuff
 
+    for (n = 0; n <= superiorQ; n += 1) {
+        if (questionArray[n] == true) {
+        } else {
+            questionArray[n] = question;
+        }
+    } */
 
 
 
